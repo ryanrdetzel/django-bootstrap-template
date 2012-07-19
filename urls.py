b90579a619b5642ab4e101b53aad830c510f12e8
Ryan Detzel <ryandetzel@gmailc.om>
2012-07-18 20:40:54 -0400

    bootstrap on the default template works

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'django_bootstrap.views.home', name='home'),
+    url(r'^$', direct_to_template, {'template': 'default.html'}, 
+        name="default"),
     # url(r'^django_bootstrap/', include('django_bootstrap.foo.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
